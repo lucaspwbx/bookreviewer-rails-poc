0f094d6d819f1f0b0bbc46a16a1fb70afd573b84
Lucas Weiblen <lucasweiblen@gmail.com>
2015-01-18 17:41:13 -0200

    add bookform and bookbox with mocked data

--- app/assets/javascripts/books.js.jsx
+++ app/assets/javascripts/books.js.jsx
@@ -27,14 +27,78 @@ var BookList = React.createClass({
   }
 });
 
-var ready = function() {
-  var books = [
-    { name: "Teste", pages: "10", language: "English" },
-    { name: "Dummies", pages: "90", language: "Deutsch" }
-  ];
+var BookForm = React.createClass({
+  clearInputs: function() {
+    this.refs.name.getDOMNode().value = '';
+    this.refs.pages.getDOMNode().value = '';
+    this.refs.language.getDOMNode().value = '';
+  },
+  handleSubmit: function() {
+    var name = this.refs.name.getDOMNode().value.trim();
+    var pages = parseInt(this.refs.pages.getDOMNode().value.trim(), 10);
+    var language = this.refs.language.getDOMNode().value.trim();
+    this.props.onNewBook({name: name, pages: pages, language: language});
+    this.clearInputs();
+    return false;
+  },
+  render: function() {
+    return (
+        <form className='bookForm' onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='book[name]'>Name:</label>
+            <input type='text' name='book[name]' ref='name' />
+          </div>
+          <div>
+            <label htmlFor='book[pages]'>Pages:</label>
+            <input type='text' name='book[pages]' ref='pages' />
+          </div>
+          <div>
+            <label htmlFor='book[language]'>Language:</label>
+            <input type='text' name='book[description]' ref='language' />
+          </div>
+          <input type='submit' value='Add book' />
+      </form>
+    );
+  }
+});
 
+var BooksBox = React.createClass({
+  getInitialState: function() {
+    return {
+      books: []
+    };
+  },
+  componentDidMount: function() {
+    this.loadBooks();
+  },
+  loadBooks: function() {
+    var books = [
+      { name: "Teste", pages: "10", language: "English" },
+      { name: "Dummies", pages: "90", language: "Deutsch" }
+    ];
+
+    this.setState({books: books});
+  },
+  handleNewBook: function(book) {
+    console.log(book);
+    var books = this.state.books;
+    var newBooks = books.concat([book]);
+    this.setState({books: newBooks});
+  },
+  render: function() {
+
+    return (
+      <div className='booksBox'>
+        <BookForm onNewBook={this.handleNewBook}/>
+        <BookList books={this.state.books}/>
+      </div>
+    );
+  }
+});
+
+var ready = function() {
   React.renderComponent(
-    <BookList books={books}/>, document.getElementById('books')
+    <BooksBox/>, document.getElementById('books')
   );
 };
 
