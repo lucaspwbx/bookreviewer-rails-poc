ffd791f3f99d96125f53c75a401853f2366dd646
Lucas Weiblen <lucasweiblen@gmail.com>
2015-01-18 14:27:48 -0200

    add resource reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,56 @@
+class ReviewsController < ApplicationController
+  def index
+    book = Book.find(params[:book_id])
+    render json: book.reviews, status: 200
+  end
+
+  def create
+    binding.pry
+    book = Book.find(params[:book_id])
+    review = Review.new(review_params)
+    if book
+      book.reviews << review
+      book.save
+      render json: review, status: 201, location: book_review_path(book, review)
+    else
+        render nothing: true, status: 422
+    end
+  end
+
+  def show
+    book = Book.find(params[:book_id])
+    review = book.reviews.find(params[:id])
+    if review
+      render json: review, status: 200
+    else
+      render nothing: true, status: 404
+    end
+  end
+
+  def update
+    book = Book.find(params[:book_id])
+    binding.pry
+    review = book.reviews.find(params[:id])
+    if review.update_attributes(review_params)
+      render json: review, status: 200
+    else
+      render nothing: true, status: 422
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    review = book.reviews.find(params[:id])
+    if review.destroy
+      head :no_content
+    else
+      render nothing: true, status: 422
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :name)
+  end
+end
